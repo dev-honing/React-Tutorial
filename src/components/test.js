9d0659198b9339dd59febec5a666a4ca2f91eaa4
Ho Nyeong <dev.honing@gmail.com>
2023-12-04 18:26:31 +0900

    기존에 작성된 클래스 컴포넌트 코드를 함수형 컴포넌트를 사용한 ES6+ 문법으로 변경 시도 -> 성공
    - 역시 코드 문법의 종류가 달라서 지원하지 않는 것이 아니었다.
    - webpack.config.js의 플러그인의 filename을 bundle.html로 바꿔서 생긴 문제였고, index.html로 바꾸니 제대로 반영되었다.

--- src/components/test.js
+++ src/components/test.js
@@ -1,11 +1,9 @@
 // src/components/test.js
 
-import React, { Component } from 'react';
+import React from 'react';
 
-class TestComponent extends Component {
-  render() {
-    return <p>This is a test component.</p>;
-  }
-}
+const TestComponent = () => {
+  return <p>This is a test component.</p>;
+};
 
 export default TestComponent;
